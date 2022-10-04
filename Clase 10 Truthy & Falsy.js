02437a937a2a036336f805226b8cc82d6a698188
dr1602 <109833006+dr1602@users.noreply.github.com>
2022-10-04 08:00:47 -0500

    Clase 10 Truthy & Falsy
    ESP. Condiciones para obtener un valor falso o verdadero en JavaScript (valores booleanos)
    ENG. Conditions to get a true or false value in JavaScript (boolean values)

---
+++ Clase 10 Truthy & Falsy.js
@@ -0,0 +1,50 @@
+/* Ejemplos de valores falsos */
+
+Boolean():
+/*False*/
+
+Boolean(0):
+/*False*/
+
+Boolean(1):
+/*True*/
+
+Boolean(null):
+/*False*/
+
+Boolean(NaN):
+/*False*/
+
+Boolean(undefined):
+/*False*/
+
+Boolean(false):
+/*False*/
+
+Boolean(""):
+/*False*/
+
+/* Ejemplos de valores verdaderos*/
+
+Boolean("a"):
+/*True*/
+
+Boolean(" "): /* string espacio */
+/*True*/
+
+Boolean("2"):
+/*True*/
+
+Boolean([]): /* array vacío */
+/*True*/
+
+Boolean({}): /* objeto vacío */
+/*True*/
+
+Boolean(function(){}): /* objeto función */
+/*True*/
+
+Boolean(True): 
+/*True*/
+
+
